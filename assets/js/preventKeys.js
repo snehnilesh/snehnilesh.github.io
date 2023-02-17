77d9fb98dcf76cf6eaf994ddd267697c768ea3fd
snehnilesh <86720256+snehnilesh@users.noreply.github.com>
2023-02-17 21:13:52 +0000

    chore

--- assets/js/preventKeys.js
+++ assets/js/preventKeys.js
@@ -1,22 +1,14 @@
 document.addEventListener("keydown", function (event) {
-    //  prevent U key
-    if(event.keyCode == 85){
+    // prevent save key combination
+    if(event.keyCode == 83 && (navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey)){
         event.preventDefault();
     }
-    //  prevent I key
-    if(event.keyCode == 73){
+    // prevent inspect key combination
+    if(event.keyCode == 73 && (navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey)){
         event.preventDefault();
     }
-    //  prevent s key
-    if(event.keyCode == 83){
-        event.preventDefault();
-    }
-    //  prevent F12 key
-    if(event.keyCode == 123){
-        event.preventDefault();
-    }
-    //  prevent F5 key
-    if(event.keyCode == 116){
+    // prevent right click
+    if(event.button == 2) {
         event.preventDefault();
     }
 });
